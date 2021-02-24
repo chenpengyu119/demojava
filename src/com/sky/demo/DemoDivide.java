a7f9fa75ee2cca703d1fdd4eff600b0724883fd9
chenpengyu <pengyuchen119@gmail.com>
2021-02-24 09:30:47 +0800

    java整除

---
+++ src/com/sky/demo/DemoDivide.java
@@ -0,0 +1,18 @@
+package com.sky.demo;
+
+/**
+ * @author pengyu
+ * @date 2021/2/24 09:04
+ */
+public class DemoDivide {
+
+    public static void main(String[] args) {
+
+        demo1();
+
+    }
+
+    private static void demo1(){
+        System.out.println(1/2);
+    }
+}
