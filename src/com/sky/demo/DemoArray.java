eac412087ab67ada57fb3e51a0c26849c586434c
chenpengyu <pengyuchen119@gmail.com>
2021-03-22 14:42:42 +0800

    feat: 测试asList不能用于处理基本类型

--- src/com/sky/demo/DemoArray.java
+++ src/com/sky/demo/DemoArray.java
@@ -1,5 +1,8 @@
 package com.sky.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author pengyu
  * @date 2020/8/5 13:12
@@ -7,7 +10,7 @@ package com.sky.demo;
 public class DemoArray {
 
     public static void main(String[] args) {
-        demo3();
+        demo4();
     }
 
     private static void demo2(){
@@ -19,5 +22,15 @@ public class DemoArray {
         String[] split = str.split(",");
         System.out.println(split.length);
         System.out.println(split[0]);
+    }
+
+
+    /**
+     * asList 基本类型,因为list的范型成了数组类型
+     */
+    private static void demo4(){
+        int[] arr = new int[]{1,2,3,4,5};
+        List<int[]> ints = Arrays.asList(arr);
+        System.out.println(ints.size());
     }
 }
