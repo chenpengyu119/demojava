590c04d1f82868ba4292d67c169998bf4c22b3ab
chenpengyu <pengyuchen119@gmail.com>
2021-03-22 14:43:04 +0800

    feat: 测试if/elseif执行顺序和逻辑

---
+++ src/com/sky/demo/DemoElseIf.java
@@ -0,0 +1,21 @@
+package com.sky.demo;
+
+/**
+ * @author pengyu
+ * @date 2021/3/15 17:11
+ */
+
+public class DemoElseIf {
+
+    public static void main(String[] args) {
+        demo1();
+    }
+
+    public static void demo1(){
+        if (true){
+            System.out.println("If");
+        }else if (true){
+            System.out.println("else if");
+        }
+    }
+}
